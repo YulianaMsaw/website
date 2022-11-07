8a392f512c643fbd778074136ce25506e84cf911
IgorTrofimenko <tro@sfedu.ru>
2022-11-07 15:39:10 +0300

    added middelware

---
+++ middleware/authMiddelware.js
@@ -0,0 +1,26 @@
+const jwt=require('jsonwebtoken')
+module.exports=function (req,res, next){
+
+
+    if(req.method==='OPTIONS'){
+
+        next()
+
+    }
+    try
+    {
+
+
+        const token=req.headers.authorization.split(' ')[1]
+        if(!token){
+            res.status(401).json({message:'Пользователь не авторизован'})
+        }
+
+        const decoded=jwt.verify(token,process.env.SECRET_KEY)
+        req.user=decoded
+        next()
+    }catch (err){
+        res.status(401).json({message:'Пользователь не авторизован'})
+    }
+
+}
---
+++ middleware/checkRoleMiddelware.js
@@ -0,0 +1,33 @@
+const jwt=require('jsonwebtoken')
+module.exports=function (role){
+
+    return function (req,res, next){
+
+
+        if(req.method==='OPTIONS'){
+
+            next()
+
+        }
+        try
+        {
+
+
+            const token=req.headers.authorization.split(' ')[1]
+            if(!token){
+                res.status(401).json({message:'Пользователь не авторизован'})
+            }
+
+            const decoded=jwt.verify(token,process.env.SECRET_KEY)
+            if(decoded.roles!==role){
+                return res.status(403).json({message: 'Нет доступа'})
+            }
+            req.user=decoded
+            next()
+        }catch (err){
+            res.status(401).json({message:'Пользователь не авторизован'})
+        }
+
+    }
+
+}
