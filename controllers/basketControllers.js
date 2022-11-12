56117a571576f0924a5b3d010d2a52b6b0990fff
IgorTrofimenko <tro@sfedu.ru>
2022-11-12 18:54:39 +0300

    add basketControllers

---
+++ controllers/basketControllers.js
@@ -0,0 +1,139 @@
+const  {Product}=require('../models/models')
+
+const  BasketControllersHelper=require('../controllers/basketControllersHelper')
+
+const ApiError=require('../error/ApiErrors')
+
+const maxAge=60 * 60 * 1000 * 24 * 365  // one year
+const signed=true
+
+
+class BasketControllers{
+
+    async getOne(req,res,next)
+    {
+
+        try {
+            let basket
+            if(req.signedCookies.basketId)
+            {
+                console.log(parseInt(req.signedCookies.basketId))
+                basket=await BasketControllersHelper.getOne(parseInt(req.signedCookies.basketId))
+                console.log(basket);
+            }else
+            {
+                console.log('g')
+                basket=await BasketControllersHelper.create()
+                console.log(basket)
+            }
+
+            res.cookie('basketId',basket.id, {maxAge,signed})
+            res.json(basket)
+        }catch (err) {
+            next(ApiError.badRequest(err.message))
+        }
+
+
+    }
+
+    async append(req, res, next) {
+        try {
+
+            let basketId
+            console.log(req.signedCookies.basketId)
+            if (!req.signedCookies.basketId) {
+                let created = await BasketControllersHelper.create()
+                basketId = created.id
+            } else {
+                basketId = parseInt(req.signedCookies.basketId)
+            }
+            const {productId, quantity} = req.params
+            const basket = await BasketControllersHelper.append(basketId, productId, quantity)
+            res.cookie('basketId', basket.id, {maxAge, signed})
+            res.json(basket)
+        } catch(err) {
+
+            next(ApiError.badRequest(err.message))
+
+        }
+    }
+
+    async increment(req, res, next) {
+        try {
+
+            let basketId
+            if (!req.signedCookies.basketId) {
+                let created = await BasketControllersHelper.create()
+                basketId = created.id
+            } else {
+                basketId = parseInt(req.signedCookies.basketId)
+            }
+            const {productId, quantity} = req.params
+            const basket = await BasketControllersHelper.increment(basketId, productId, quantity)
+            res.cookie('basketId', basket.id, {maxAge, signed})
+            res.json(basket)
+        } catch(err) {
+
+            next(ApiError.badRequest(err.message))
+
+        }
+    }
+
+    async decrement(req, res, next) {
+        try {
+
+            let basketId
+            if (!req.signedCookies.basketId) {
+                let created = await BasketControllersHelper.create()
+                basketId = created.id
+            } else {
+                basketId = parseInt(req.signedCookies.basketId)
+            }
+            const {productId, quantity} = req.params
+            const basket = await BasketControllersHelper.decrement(basketId, productId, quantity)
+            res.cookie('basketId', basket.id, {maxAge, signed})
+            res.json(basket)
+        } catch(err) {
+
+            next(ApiError.badRequest(err.message))
+        }
+    }
+
+    async remove(req, res, next) {
+        try {
+            let basketId
+            if (!req.signedCookies.basketId) {
+                let created = await BasketControllersHelper.create()
+                basketId = created.id
+            } else {
+                basketId = parseInt(req.signedCookies.basketId)
+            }
+            const basket = await BasketControllersHelper.remove(basketId, req.params.productId)
+            res.cookie('basketId', basket.id, {maxAge, signed})
+            res.json(basket)
+        } catch(err) {
+            next(ApiError.badRequest(err.message))
+        }
+    }
+
+    async clear(req, res, next) {
+        try {
+            let basketId
+            if (!req.signedCookies.basketId) {
+                let created = await BasketControllersHelper.create()
+                basketId = created.id
+            } else {
+                basketId = parseInt(req.signedCookies.basketId)
+            }
+            const basket = await BasketControllersHelper.clear(basketId)
+            res.cookie('basketId', basket.id, {maxAge, signed})
+            res.json(basket)
+        } catch(err) {
+
+            next(ApiError.badRequest(err.message))
+        }
+    }
+
+}
+
+module.exports=new BasketControllers()
---
+++ controllers/basketControllersHelper.js
@@ -0,0 +1,165 @@
+
+const {Product}= require('../models/models')
+const {Basket}= require('../models/models')
+const {BasketProduct}= require('../models/models')
+const  ApiError=require('../error/ApiErrors')
+
+
+
+
+const pretty = (basket) => {
+    const data = {}
+    data.id = basket.id
+    data.products = []
+    if (basket.products) {
+        data.products = basket.products.map(item => {
+            return {
+                id: item.id,
+                name: item.name,
+                price: item.price,
+                quantity: item.basket_product.quantity
+            }
+        })
+    }
+    return data
+}
+
+
+class BasketControllersHelper {
+
+    async getOne(basketId) {
+
+        let basket = await Basket.findByPk(basketId, {
+            attributes: ['id'],
+            include: [
+                {model: Product, attributes: ['id', 'name', 'price','rating','img']},
+            ],
+        })
+
+        console.log('hor')
+        console.log(basket)
+        if (!basket) {
+            basket = await Basket.create()
+        }
+        return pretty(basket)
+    }
+
+    async create() {
+        const basket = await Basket.create()
+        return pretty(basket)
+    }
+
+    async append(basketId, productId, quantity) {
+        let basket = await Basket.findByPk(basketId, {
+            attributes: ['id'],
+            include: [
+                {model: Product, attributes: ['id', 'name', 'price']},
+            ]
+        })
+        if (!basket) {
+            basket = await Basket.create()
+        }
+// проверяем, есть ли уже этот товар в корзине
+        const basket_product = await BasketProduct.findOne({
+            where: {basketId, productId}
+        })
+        if (basket_product) { // есть в корзине
+            await basket_product.increment('quantity', {by: quantity})
+        } else { // нет в корзине
+            await BasketProduct.create({basketId, productId, quantity})
+        }
+
+        await basket.reload()
+        return pretty(basket)
+    }
+
+async increment(basketId, productId, quantity) {
+    let basket = await Basket.findByPk(basketId, {
+        include: [{model: Product, as: 'products'}]
+    })
+    if (!basket) {
+        basket = await Basket.create()
+    }
+    // проверяем, есть ли этот товар в корзине
+    const basket_product = await BasketProduct.findOne({
+        where: {basketId, productId}
+    })
+    if (basket_product) {
+        await basket_product.increment('quantity', {by: quantity})
+        // обновим объект корзины, чтобы вернуть свежие данные
+        await basket.reload()
+    }
+    return pretty(basket)
+}
+
+async decrement(basketId, productId, quantity) {
+    let basket = await BasketMapping.findByPk(basketId, {
+        include: [{model: ProductMapping, as: 'products'}]
+    })
+    if (!basket) {
+        basket = await Basket.create()
+    }
+    // проверяем, есть ли этот товар в корзине
+    const basket_product = await BasketProductMapping.findOne({
+        where: {basketId, productId}
+    })
+    if (basket_product) {
+        if (basket_product.quantity > quantity) {
+            await basket_product.decrement('quantity', {by: quantity})
+        } else {
+            await basket_product.destroy()
+        }
+        // обновим объект корзины, чтобы вернуть свежие данные
+        await basket.reload()
+    }
+    return pretty(basket)
+}
+
+async remove(basketId, productId) {
+    let basket = await Basket.findByPk(basketId, {
+        include: [{model: Product, as: 'products'}]
+    })
+    if (!basket) {
+        basket = await Basket.create()
+    }
+    // проверяем, есть ли этот товар в корзине
+    const basket_product = await BasketProduct.findOne({
+        where: {basketId, productId}
+    })
+    if (basket_product) {
+        await basket_product.destroy()
+        // обновим объект корзины, чтобы вернуть свежие данные
+        await basket.reload()
+    }
+    return pretty(basket)
+}
+
+async clear(basketId) {
+    let basket = await Basket.findByPk(basketId, {
+        include: [{model: Product, as: 'products'}]
+    })
+    if (basket) {
+        await BasketProduct.destroy({where: {basketId}})
+        // обновим объект корзины, чтобы вернуть свежие данные
+        await basket.reload()
+    } else {
+        basket = await Basket.create()
+    }
+    return pretty(basket)
+}
+
+async delete(basketId) {
+    const basket = await Basket.findByPk(basketId, {
+        include: [{model: Product, as: 'products'}]
+    })
+    if (!basket) {
+        throw new Error('Корзина не найдена в БД')
+    }
+    await basket.destroy()
+    return pretty(basket)
+}
+}
+
+
+module.exports=new BasketControllersHelper()
+
